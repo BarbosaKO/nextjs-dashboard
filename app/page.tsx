d65a510cda156b1f872ad32320194ac6bb6d5e89
BarbosaKO <gahbarbosasilva@gmail.com>
2024-04-03 17:38:12 -0300

    Authentication added and color modifield

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import AcmeLogo from '@/app/ui/acme-logo';
+import PlasmaLogo from '@/app/ui/plasma-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
@@ -7,22 +7,22 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <AcmeLogo />
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-purple-500 p-4 md:h-52">
+        <PlasmaLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"/>
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
+            <strong>Welcome to <span className="line-through">Acme</span> Plasma.</strong> This is the example for the{' '}
+            <a href="https://nextjs.org/learn/" className="text-purple-500">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
+            , brought by Vercel and created by <a href="https://github.com/BarbosaKO" target="_blank" className="text-orange-500">Gabriel Barbosa</a>.
           </p>
           <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            className="flex items-center gap-5 self-start rounded-lg bg-purple-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-purple-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,10 +12,10 @@ const config: Config = {
         '13': 'repeat(13, minmax(0, 1fr))',
       },
       colors: {
-        blue: {
-          400: '#2589FE',
-          500: '#0070F3',
-          600: '#2F6FEB',
+        purple: {
+          400: '#c084fc',
+          500: '#a855f7',
+          600: '#9333ea',
         },
       },
     },
