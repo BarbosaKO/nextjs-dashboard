b0e83015f3c18b144d2ca6daf74009ac625a3b41
BarbosaKO <gahbarbosasilva@gmail.com>
2024-04-03 18:53:31 -0300

    auth configuration

--- auth.config.ts
+++ auth.config.ts
@@ -8,11 +8,12 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      const isOnRoot = nextUrl.pathname === '/';
       
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
+      } else if (isLoggedIn && !isOnRoot) {
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
